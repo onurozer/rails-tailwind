3c590c98e6cfffca8c49b7a8d28a05e88dc26a15
Onur Ozer <onurozer@gmail.com>
2021-03-20 18:05:11 +0100

    added tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  plugins: [
+  plugins: [    
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
+    require("tailwindcss"),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
